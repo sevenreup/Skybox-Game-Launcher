fe9cc75a6404390d4307179ef2f178f120ed98ea
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-06-02 19:41:37 +0200

    repo stuff

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+
+export default class extends PureComponent {
+
+    render() {
+        return (
+            <div>Home</div>
+        )
+    }
+}
---
+++ src/pages/Settings.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+
+
+export default class Settings extends PureComponent {
+    render() {
+        return (
+            <div>settings</div>
+        )
+    }
+}
