0d6982c1108fb3c46acff7a4c1f4c74d6cf61e96
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-06-13 22:41:20 +0200

    hero

--- src/components/HeroNormal/HeroNormal.js
+++ src/components/HeroNormal/HeroNormal.js
@@ -1,23 +1,29 @@
 import React, { PureComponent } from "react";
-import { withStyles } from "@material-ui/core";
-
+import { withStyles, ButtonBase } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import './heronormal.scss';
+const heroHeight = 350;
 const style = theme => ({
     image: {
         display: 'block',
         boxSizing: 'border-box',
         background: 'url(image/image2.jpg) no-repeat',
         width: '100%',
-        height: 300,
+        height: heroHeight,
         objectFit: 'cover',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
     },
     imageBack: {
-        background: 'black',
-        opacity: 0.3,
+        background: '#0000006b',
         width: '100%',
-        height: 300,
+        height: heroHeight,
         objectFit: 'cover',
+        overflow: 'hidden'
+    },
+    play: {
+        background: '#007bff',
     }
 });
 
@@ -27,11 +33,20 @@ class HeroMain extends PureComponent {
     }
     render() {
         const { classes } = this.props;
-        return <div data-scroll>
+        return <div data-scroll className="hero">
             <div data-scroll-content>
                 <div className="item">
                     <figure className={classes.image}>
-                        <div className={classes.imageBack}/>
+                        <div className={classes.imageBack}>
+                            <div className="hero-caption">
+                                <span className="game-pill">Last Played</span>
+                                <h1 className="hero-title">Random Game that you didnt play</h1>
+                                <ButtonBase focusRipple className={`btn-pill-icon ${classes.play}`}>
+                                    <FontAwesomeIcon icon={faPlayCircle} className="btn-icon"/>
+                                    <span className="btn-caption" >Play</span>
+                                </ButtonBase>
+                            </div>
+                        </div>
                     </figure>
                 </div>
 
